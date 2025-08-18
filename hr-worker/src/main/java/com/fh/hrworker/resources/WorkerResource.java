af2730227a3292fdf94d84e1f4af27a853ab51ed
FabioHelmer <fabio.helmer.kuhn@gmail.com>
2025-08-18 12:12:41 -0300

    feat: hr-worker configurando actuator runtime

--- hr-worker/src/main/java/com/fh/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/fh/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.fh.hrworker.repositories.WorkerRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping(value = "/workers")
 @Log4j2
 public class WorkerResource {
