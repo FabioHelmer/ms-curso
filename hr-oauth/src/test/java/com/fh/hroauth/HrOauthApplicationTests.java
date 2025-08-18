5ce03c69d59b28151dabec5772660ed374bbe07d
FabioHelmer <fabio.helmer.kuhn@gmail.com>
2025-08-18 17:18:49 -0300

    chore: iniciando projeto hr-oauth

---
+++ hr-oauth/src/test/java/com/fh/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
